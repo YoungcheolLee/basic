3a12affd12f161a960facacff05b29df3094401a
youngcheolLee <zodiac0905@gmail.com>
2022-12-27 22:09:39 +0900

    create myValues.js

---
+++ basic/myValues.js
@@ -0,0 +1,31 @@
+/**
+ * 자바스크립트 기본 Value 정의서
+ */
+
+// 원시타입(Primitive data type)
+// 1. Number
+let myNum = 777;
+
+// 2. String
+let myStr = "Photato";
+console.log(`Primitive data type: String = ${myStr}`);
+
+// 3. boolean : true
+let trueBool = "true";
+console.log(`Primitive data type: Boolean = ${trueBool}`);
+
+// 3. boolean : false
+let falseBool = "false";
+console.log(`Primitive data type: Boolean = ${falseBool}`);
+
+// 4. null
+let myNull = null;
+console.log(`Primitive data type: null = ${myNull}`);
+
+// 5. undefined
+let myUndefined = undefined;
+console.log(`Primitive data type: null = ${myUndefined}`);
+
+// 6. symbol
+let mySymbol = Symbol(1);
+console.log(mySymbol);
