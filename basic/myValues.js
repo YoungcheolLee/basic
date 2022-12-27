c0ab4cdeccdfcfff1fcb7d48d635729ba81704a6
youngcheolLee <zodiac0905@gmail.com>
2022-12-28 02:22:46 +0900

    Update myValues.js

--- basic/myValues.js
+++ basic/myValues.js
@@ -39,3 +39,25 @@ let myObject = { name: "LeeYoungCheol", age: 1989 };
 console.log(
   `Primitive data type: object in name = ${myObject.name} , age = ${myObject.age}`
 );
+
+let myArray = ["a", "b", "c"];
+console.log(`배열 리터럴 myArray = ${myArray}`);
+
+/**
+ * 정규 표현식 리터럴(Literal)
+ * 1. 정규 표현식은 "/"로 감싸진 패턴을 리터럴로 사용한다.
+ * 2. 리터럴은 데이터 그 자체를 의미한다.
+ * 3. 상수(Const) =변하지 않는 변수를 의미. (메모리 위치가 고정)
+ *    리터럴은 변수의 값이 변하지 않는 데이터 (메모리 위치 안의 데이터가 고정)
+ */
+let myVal = /ab+c^/;
+console.log(`정규표현식 myVal = ${myVal}`);
+
+// 함수 리터럴
+let myFunc = function () {
+  let a = 1;
+  let b = 2;
+  return a + b;
+};
+
+console.log(`함수 리터럴 myFunc = ${myFunc()}`);
